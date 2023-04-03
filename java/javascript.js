a24278a80220ea089e8936b34544d00f598d01fb
JLeandroS <105645222+JLeandroS@users.noreply.github.com>
2023-04-03 19:43:46 -0300

    Update javascript.js

--- java/javascript.js
+++ java/javascript.js
@@ -5,10 +5,10 @@ var menu_modal = document.getElementById('menu_modal');
 $('#btn_menu').click( function(){
     btn_menu.style.display = "none";
     btn_menu_close.style.display = "block";
-    menu_modal.style.display = "";
+    menu_modal.style.transform = "translateX(0%)";
 })
 $('#btn_menu_close').click( function(){
     btn_menu.style.display = "";
     btn_menu_close.style.display = "none";
-    menu_modal.style.display = "none";
+    menu_modal.style.transform = "translateX(100%)";
 })
