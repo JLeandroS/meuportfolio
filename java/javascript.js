fbe4148303fdd5e6e4a604f867a3cddbc4b1de59
JLeandroS <105645222+JLeandroS@users.noreply.github.com>
2025-07-28 16:26:51 -0300

    Update javascript.js

--- java/javascript.js
+++ java/javascript.js
@@ -2,6 +2,11 @@ var btn_menu = document.getElementById('btn_menu');
 var btn_menu_close = document.getElementById('btn_menu_close');
 var menu_modal = document.getElementById('menu_modal');
 
+var btn-gup = document.getElementById('btn-gup');
+var btn-fisio = document.getElementById('btn-fisio');
+var btn-optimize = document.getElementById('btn-optimize');
+var btn-logistica = document.getElementById('btn-logistica');
+
 $('#btn_menu').click( function(){
     btn_menu.style.display = "none";
     btn_menu_close.style.display = "block";
@@ -12,3 +17,28 @@ $('#btn_menu_close').click( function(){
     btn_menu_close.style.display = "none";
     menu_modal.style.transform = "translateX(100%)";
 })
+
+$('#btn-gup').click( function(){
+    hide_data();
+    btn-gup.style.display = '';
+})
+
+$('#btn-fisio').click( function(){
+    hide_data();
+    btn-fisio.style.display = '';
+})
+$('#btn-optimize').click( function(){
+    hide_data();
+    btn-optimize.style.display = '';
+})
+$('#btn-logistica').click( function(){
+    hide_data();
+    btn-logistica.style.display = '';
+})
+
+function hide_data(){
+    btn-gup.style.display = 'none';
+    btn-logistica.style.display = 'none';
+    btn-fisio.style.display = 'none';
+    btn-optimize.style.display = 'none';
+}
