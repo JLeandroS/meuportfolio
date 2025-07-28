120041011b2088cfce837cab139d92b1a9e0f32d
JLeandroS <105645222+JLeandroS@users.noreply.github.com>
2025-07-28 16:35:51 -0300

    Update javascript.js

--- java/javascript.js
+++ java/javascript.js
@@ -2,10 +2,10 @@ var btn_menu = document.getElementById('btn_menu');
 var btn_menu_close = document.getElementById('btn_menu_close');
 var menu_modal = document.getElementById('menu_modal');
 
-var btn_gup = document.getElementById('btn-gup');
-var btn_fisio = document.getElementById('btn-fisio');
-var btn_optimize = document.getElementById('btn-optimize');
-var btn_logistica = document.getElementById('btn-logistica');
+var btn_gup = document.getElementById('data-gup');
+var btn_fisio = document.getElementById('data-fisio');
+var btn_optimize = document.getElementById('data-optimize');
+var btn_logistica = document.getElementById('data-logistica');
 
 $('#btn_menu').click( function(){
     btn_menu.style.display = "none";
